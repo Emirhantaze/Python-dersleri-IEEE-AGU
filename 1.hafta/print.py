af13649d3030ba8dc6c5c4e7301f5e6134373e23
Emirhan Taze <emtiyltezkry@gmail.com>
2020-12-04 13:58:15 +0300

    Update print.py

--- 1.hafta/print.py
+++ 1.hafta/print.py
@@ -11,3 +11,15 @@ print("hello world")  # <<<< doğru kullanım
 hello_world = "hello world"
 
 print(hello_world)  # farklı bir kullnım tarzı
+
+"""
+
+bu kullanımda print edilen dğerein sonuna mutalaka "\n"  "\n\r" gibi satır sonu byteları eklenir bunu değiştirmek için
+end parametresini "" değerine eşitleyebiliriz
+"""
+
+print("hello")
+print("world")
+# satır atladı
+print("hello", end=" ")
+print("world")
